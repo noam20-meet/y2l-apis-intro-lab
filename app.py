0e89859b31a9c99994cd1fc9b0d6ed073c865fc6
myname21-meet <myname21@meet.mit.edu>
2019-12-12 11:44:40 -0500

    apislab

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 ############################
 ### Lab Part 1: JSON 
@@ -13,52 +13,53 @@ def home():
 
 @app.route('/movie')
 def movies():
-    json_string = """
-                    {
-                    "title" : "Black Panther", 
-                    "releaseDate" : "2/16/2018",
-                    "image_url": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/KXC1W2-920x584.jpg"
-                    }
-                    """
-    return render_template('movie.html', movie={})
+	json_string = """
+					{
+					"title" : "Black Panther", 
+					"releaseDate" : "2/16/2018",
+					"image_url": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/KXC1W2-920x584.jpg"
+					}
+					"""
+	parsed_json = json.loads(json_string)
+	return render_template('movie.html', movie=parsed_json)
 
 
 @app.route('/tvshows')
 def tv_shows():
-    json_string = """
-    [{
-    "url":"http://www.tvmaze.com/shows/2705/narcos",
-    "name":"Narcos",
-    "language":"English",
-    "genres":[  
-      "Drama",
-      "Crime"
-    ]},
-    {  
-    "url":"http://www.tvmaze.com/shows/305/black-mirror",
-    "name":"Black Mirror",
-    "language":"English",
-    "genres":[  
-        "Drama",
-        "Science-Fiction",
-        "Thriller"
+	json_string = """
+	[{
+	"url":"http://www.tvmaze.com/shows/2705/narcos",
+	"name":"Narcos",
+	"language":"English",
+	"genres":[  
+	  "Drama",
+	  "Crime"
+	]},
+	{  
+	"url":"http://www.tvmaze.com/shows/305/black-mirror",
+	"name":"Black Mirror",
+	"language":"English",
+	"genres":[  
+		"Drama",
+		"Science-Fiction",
+		"Thriller"
    ]},
    {  
-    "url":"http://www.tvmaze.com/shows/305/black-mirror",
-    "name":"Black Mirror",
-    "type":"Scripted",
-    "language":"English",
-    "genres":[  
-        "Drama",
-        "Science-Fiction",
-        "Thriller"
-    ]
-    }]    
-    """
-    # Write code here to take the `json_string` and return list of movies to the user
+	"url":"http://www.tvmaze.com/shows/305/black-mirror",
+	"name":"Black Mirror",
+	"type":"Scripted",
+	"language":"English",
+	"genres":[  
+		"Drama",
+		"Science-Fiction",
+		"Thriller"
+	]
+	}]    
+	"""
+	# Write code here to take the `json_string` and return list of movies to the user
 
 
-    return render_template('tv_shows.html')
+	return render_template('tv_shows.html')
 
 
 ############################
@@ -66,15 +67,12 @@ def tv_shows():
 ############################
 @app.route('/dogs')
 def dog_breeds():
-    """
-    If you visit https://dog.ceo/api/breeds/list/all 
-    a list of all dog breeds is returned. Try this in your browser! (Chrome/firefox)
 
-    Using the `requests` library (as shown in the slides)
-    Do a GET request to the link above to get all dog breeds and return them
-    to them as a list to the user as a bullet pointed list
-    """
-    return render_template('dogs.html')
+	response = requests.get(" https://dog.ceo/api/breeds/list/all")
+	parsed_content = json.loads(response.content)
+	list_dogs = parsed_content["message"]
+	return render_template('dogs.html', list_dogs=list_dogs)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
